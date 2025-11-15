ca4deace00a2b964c0c41f3a85c85e4028a7e0eb
Ngọc Đan <danngoc6789@gmail.com>
2025-11-16 00:26:55 +0700

    updates

---
+++ Controllers/PolicyController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoWeb.Controllers
+{
+    public class PolicyController : Controller
+    {
+        // GET: Policy
+        public ActionResult ThuDoi()
+        {
+            return View();
+        }
+        public ActionResult BaoHanh()
+        {
+            return View();
+        }
+        public ActionResult GiaoHang()
+        {
+            return View();
+        }
+        public ActionResult QA()
+        {
+            return View();
+        }
+    }
+}
